0d601aa9ec7500430f1be40476c0c4dc1def2cf1
sz16900 <sz16900@my.bristol.ac.uk>
2020-07-07 13:17:17 -0500

    :arrow_up: initialize deps
    npm initialize

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        // Images
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        // Font
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+      // Rules required for Bootstrap to work
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader',
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader',
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            /* eslint-disable */
+            options: {
+              plugins() {
+                return [require('autoprefixer')];
+              },
+            },
+            /* eslint-enable */
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
