d027fd3869c78bdf5dc33c65a245d31f91073421
sz16900 <sz16900@my.bristol.ac.uk>
2020-07-09 13:41:46 -0500

    error handling

--- src/getWeatherData.js
+++ src/getWeatherData.js
@@ -1,41 +1,44 @@
-// Use this for images
-// http://openweathermap.org/img/wn/02n@2x.png
 const getWeatherData = async (info) => {
   const response = await fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=${info}&units=imperial&APPID=89aa0327608e9bae4e9fcb1f777577f9`,
-    { mode: 'cors' }
+    { mode: 'cors' },
   );
 
-  const gatheredData = {};
-  const weatherData = await response.json();
-  console.log(weatherData);
+  if (!response.ok) {
+    throw new Error(
+      'Something went wrong with your query. Perhaps you mistyped a city?',
+    );
+  } else {
+    const gatheredData = {};
+    const weatherData = await response.json();
 
-  //   city & country
-  gatheredData.city = weatherData.name;
-  gatheredData.country = weatherData.sys.country;
-  //   temperature
-  gatheredData.temperature = weatherData.main.temp;
-  //   current time
-  let sec = weatherData.dt;
-  let date = new Date(sec * 1000);
-  let timestr = date.toLocaleTimeString();
-  gatheredData.current_time = timestr;
-  //   main condition
-  gatheredData.icon = weatherData.weather[0].icon;
-  gatheredData.description = weatherData.weather[0].description;
-  //   humidity
-  gatheredData.humidity = weatherData.main.humidity;
-  //   sunrise
-  sec = weatherData.sys.sunrise;
-  date = new Date(sec * 1000);
-  timestr = date.toLocaleTimeString();
-  gatheredData.sunrise = timestr;
-  //   sunset
-  sec = weatherData.sys.sunset;
-  date = new Date(sec * 1000);
-  timestr = date.toLocaleTimeString();
-  gatheredData.sunset = timestr;
-  return gatheredData;
+    //   city & country
+    gatheredData.city = weatherData.name;
+    gatheredData.country = weatherData.sys.country;
+    //   temperature
+    gatheredData.temperature = weatherData.main.temp;
+    //   current time
+    let sec = weatherData.dt;
+    let date = new Date(sec * 1000);
+    let timestr = date.toLocaleTimeString();
+    gatheredData.current_time = timestr;
+    //   main condition
+    gatheredData.icon = weatherData.weather[0].icon;
+    gatheredData.description = weatherData.weather[0].description;
+    //   humidity
+    gatheredData.humidity = weatherData.main.humidity;
+    //   sunrise
+    sec = weatherData.sys.sunrise;
+    date = new Date(sec * 1000);
+    timestr = date.toLocaleTimeString();
+    gatheredData.sunrise = timestr;
+    //   sunset
+    sec = weatherData.sys.sunset;
+    date = new Date(sec * 1000);
+    timestr = date.toLocaleTimeString();
+    gatheredData.sunset = timestr;
+    return gatheredData;
+  }
 };
 
 export default getWeatherData;
--- src/renderData.js
+++ src/renderData.js
@@ -1,25 +1,26 @@
 import getWeatherData from './getWeatherData';
 
 const renderData = (info) => {
-  getWeatherData(info).then((data) => {
-    console.log(data);
-    const h1 = document.getElementById('city-country');
-    h1.textContent = `${data.city}, ${data.country}`;
-    const temperature = document.getElementById('temperature');
-    temperature.textContent = `${Math.floor(data.temperature)}°`;
-    const span = document.getElementById('icon');
-    span.src = `http://openweathermap.org/img/wn/${data.icon}@2x.png`;
-    const time = document.getElementById('card-time');
-    time.textContent = data.current_time;
-    const description = document.getElementById('description');
-    description.textContent = data.description;
-    const humidity = document.getElementById('humidity');
-    humidity.textContent = `Humidity: ${data.humidity}%`;
-    const sunrise = document.getElementById('sunrise-time');
-    sunrise.textContent = data.sunrise;
-    const sunset = document.getElementById('sunset-time');
-    sunset.textContent = data.sunset;
-  });
+  getWeatherData(info)
+    .then((data) => {
+      const h1 = document.getElementById('city-country');
+      h1.textContent = `${data.city}, ${data.country}`;
+      const temperature = document.getElementById('temperature');
+      temperature.textContent = `${Math.floor(data.temperature)}°`;
+      const span = document.getElementById('icon');
+      span.src = `http://openweathermap.org/img/wn/${data.icon}@2x.png`;
+      const time = document.getElementById('card-time');
+      time.textContent = data.current_time;
+      const description = document.getElementById('description');
+      description.textContent = data.description;
+      const humidity = document.getElementById('humidity');
+      humidity.textContent = `Humidity: ${data.humidity}%`;
+      const sunrise = document.getElementById('sunrise-time');
+      sunrise.textContent = data.sunrise;
+      const sunset = document.getElementById('sunset-time');
+      sunset.textContent = data.sunset;
+    })
+    .catch((e) => console.log(e));
 };
 
 export default renderData;
